f93bdeff7901df2be11c2acc71d365a31be85daa
liulihua <hc_liulihua@163.com>
2025-03-15 15:42:43 +0800

    feat: 添加严格模式

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,62 +1,58 @@
-
-import { Button, Checkbox, Form, Input, Flex } from 'antd';
+'use client'
+import { Button, Checkbox, Form, Input, Flex } from 'antd'
 
 const Login = () => {
-    const onFinish = (values) => {
-        console.log('Received values of form: ', values);
-      };
-    return (
-            <Form
-            name="login"
-            initialValues={{
-            remember: true,
-            }}
-            style={{
-            maxWidth: 360,
-            }}
-            onFinish={onFinish}
-        >
-            <Form.Item
-            name="username"
-            rules={[
-                {
-                required: true,
-                message: 'Please input your Username!',
-                },
-            ]}
-            >
-            <Input  placeholder="Username" />
-            </Form.Item>
-            <Form.Item
-            name="password"
-            rules={[
-                {
-                required: true,
-                message: 'Please input your Password!',
-                },
-            ]}
-            >
-            <Input  type="password" placeholder="Password" />
-            </Form.Item>
-            <Form.Item>
-            <Flex justify="space-between" align="center">
-                <Form.Item name="remember" valuePropName="checked" noStyle>
-                <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-            
-            </Flex>
-            </Form.Item>
+  const onFinish = values => {
+    console.log('Received values of form: ', values)
+  }
+  return (
+    <Form
+      name='login'
+      initialValues={{
+        remember: true
+      }}
+      style={{
+        maxWidth: 360
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name='username'
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Username!'
+          }
+        ]}
+      >
+        <Input placeholder='Username' />
+      </Form.Item>
+      <Form.Item
+        name='password'
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Password!'
+          }
+        ]}
+      >
+        <Input type='password' placeholder='Password' />
+      </Form.Item>
+      <Form.Item>
+        <Flex justify='space-between' align='center'>
+          <Form.Item name='remember' valuePropName='checked' noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+        </Flex>
+      </Form.Item>
 
-            <Form.Item>
-            <Button block type="primary" htmlType="submit">
-                Log in
-            </Button>
-        
-            </Form.Item>
-        </Form>
-
-    )
-
+      <Form.Item>
+        <Button block type='primary' htmlType='submit'>
+          Log in
+        </Button>
+      </Form.Item>
+    </Form>
+  )
 }
 
 export default Login
